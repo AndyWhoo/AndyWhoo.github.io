283be18a846f4bff15fd50924f05c8b885e23f76
AndyWhoo <33040315+AndyWhoo@users.noreply.github.com>
2018-03-22 12:29:29 -0700

    added super mario clouds

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,12 +1,29 @@
-let hello = "hi, it's sunny outside";
+let clouds;
+let x=0;
+
+function preload() {
+  // you will need to change this to the file path to your image
+  clouds = loadImage('superMarioClouds_0.png');
+}
 
 function setup() {
-	console.log(hello);
-
+  createCanvas(500, 500);
+  background(200);
 }
 
-function draw() {
-	hello='hey';
+  function draw() {
+    background(200);
+    image(clouds,x,10, clouds.width/2, clouds.height/2);
+    image(clouds,x,100, clouds.width/2, clouds.height/2);
+    x++;
+  }
+
+  // Top-left corner of the img is at (0, 0)
+  // Width and height are the img's original width and height
 
 
-}
+  // If you want to scale the image to 100 x 100 pixels
+  // image(img, 0, 0, 100, 100);
+
+  // If you want to scale image by 50%
+  // image(img, 10, 10, img.width/2, img.height/2);
