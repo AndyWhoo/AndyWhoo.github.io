c9348b7b9cc588a26b4d0338c81e2807bbe07c1d
AndyWhoo <33040315+AndyWhoo@users.noreply.github.com>
2018-03-22 13:33:37 -0700

    changed stuff

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,5 +1,10 @@
+
+
+
+
 let clouds;
 let x=0;
+let speed = 5;
 
 function preload() {
   // you will need to change this to the file path to your image
@@ -8,22 +13,18 @@ function preload() {
 
 function setup() {
   createCanvas(500, 500);
-  background(200);
+ background(200);
 }
-
-  function draw() {
+function draw() {
     background(200);
     image(clouds,x,10, clouds.width/2, clouds.height/2);
-    image(clouds,x,100, clouds.width/2, clouds.height/2);
-    x++;
-  }
+   x=x+ speed;
+   move()
+ }
 
-  // Top-left corner of the img is at (0, 0)
-  // Width and height are the img's original width and height
-
-
-  // If you want to scale the image to 100 x 100 pixels
-  // image(img, 0, 0, 100, 100);
-
-  // If you want to scale image by 50%
-  // image(img, 10, 10, img.width/2, img.height/2);
+ function move(){
+   if (x>=width){
+     speed=-speed;
+   }
+   else if (x<0){speed=-speed}
+ }
