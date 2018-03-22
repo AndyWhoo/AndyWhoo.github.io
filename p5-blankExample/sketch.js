5428d14b29f571f5c926c2bdfd247e020888b322
AndyWhoo <33040315+AndyWhoo@users.noreply.github.com>
2018-03-22 14:26:25 -0700

    bounce back

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,30 +1,29 @@
-
-
-
-
 let clouds;
-let x=0;
+let x = 0;
 let speed = 5;
 
 function preload() {
-  // you will need to change this to the file path to your image
   clouds = loadImage('superMarioClouds_0.png');
 }
 
-function setup() {
-  createCanvas(500, 500);
- background(200);
-}
-function draw() {
+  function setup() {
+    createCanvas(500, 500);
     background(200);
-    image(clouds,x,10, clouds.width/2, clouds.height/2);
-   x=x+ speed;
-   move()
- }
+  }
 
- function move(){
-   if (x>=width){
-     speed=-speed;
-   }
-   else if (x<0){speed=-speed}
- }
+  function draw() {
+    background(200);
+    image(clouds, x, 10, clouds.width / 2, clouds.height / 2);
+    image(clouds, x, 100, clouds.width / 2, clouds.height / 2);
+    x = x + speed;
+    move()
+  }
+
+
+  function move() {
+    if (x >= width) {
+      speed = -speed;
+    } else if (x < 0) {
+      speed = -speed
+    }
+  }
