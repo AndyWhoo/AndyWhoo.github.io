f27e06c4a66d3d4088660591efcc5149e5c23630
AndyWhoo <33040315+AndyWhoo@users.noreply.github.com>
2018-03-22 22:30:43 -0700

    added wario

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,33 +1,45 @@
-let clouds;
+let sun;
 let x = 255;
 let speed = 5;
 var wario;
 
 function preload() {
-  clouds = loadImage('angry sun.png');
-  wario=loadGif('wariosneeze.gif');
+  sun = loadImage('angry sun.png');
+  wario = loadGif('wariosneeze.gif');
 }
 
 
-  function setup() {
-    createCanvas(500, 500);
-    background(200);
+function setup() {
+  createCanvas(500, 500);
+  background(200);
+}
+
+function draw() {
+  background(200);
+  image(sun, x, 10, sun.width / 2, sun.height / 2);
+  image(wario, 0, 0);
+
+  if (mouseIsPressed) {
+    wario.resize(0,0);
   }
+else if (background(200)){ image(wario,0,0);}
 
-  function draw() {
-    background(200);
-    image(clouds, x, 10, clouds.width / 2, clouds.height / 2);
-    image(wario,0,0);
 
-    x = x + speed;
-    move()
+
+
+
+
+  x = x + speed;
+  move()
+}
+
+
+
+
+function move() {
+  if (x >= width) {
+    speed = -speed;
+  } else if (x < -255) {
+    speed = -speed
   }
-
-
-  function move() {
-    if (x >= width) {
-      speed = -speed;
-    } else if (x < -255) {
-      speed = -speed
-    }
-  }
+}
